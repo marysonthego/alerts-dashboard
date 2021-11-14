77a353841f406ed77d60495ee16a26d4c89d38c7
marysonthego <marysonthego@gmail.com>
2021-11-14 16:18:27 -0700

    set baseUrl in heroku config vars

--- dbConfig.js
+++ dbConfig.js
@@ -7,7 +7,11 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PGuser}:${process.env.PGpassword}@${process.env.PGhost}:${process.env.PGport}/${process.env.PGdatabase}`;
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.PGhost : connectionString
-});
+      connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+  });
+
+// const pool = new Pool({
+//     connectionString: isProduction ? process.env.PGhost : connectionString
+// });
 
 module.exports = { pool };
